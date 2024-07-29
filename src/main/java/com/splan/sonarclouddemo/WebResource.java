d9bf93d1f5c85360bf2d0f75dfe6f9d61b17a1f1
RatnalaSaiCharan <RatnalaSaiCharan@host.docker.internal>
2024-07-29 18:29:52 +0530

    seventh commit

--- src/main/java/com/splan/sonarclouddemo/WebResource.java
+++ src/main/java/com/splan/sonarclouddemo/WebResource.java
@@ -1,5 +1,10 @@
 package com.splan.sonarclouddemo;
 
+import java.io.BufferedReader;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController()
 @RequestMapping("/api")
 public class WebResource {
-	
+
 	@GetMapping("/getMessage")
-	public void get() {
-	System.out.println("test");
+	public void Games_zone() {
+
+		String nullString = null;
+
+		System.out.println(nullString.length());
+
 	}
+	public void resourceLeak() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("somefile.txt"))) {
+            String line = reader.readLine();
+            System.out.println(line);
+            // Reader is not closed
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
