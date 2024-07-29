6d361e5238a6af5a1d84becf3b783987cbd668a9
RatnalaSaiCharan <RatnalaSaiCharan@host.docker.internal>
2024-07-29 18:18:02 +0530

    sixth commit

--- src/main/java/com/splan/sonarclouddemo/WebResource.java
+++ src/main/java/com/splan/sonarclouddemo/WebResource.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class WebResource {
 	
 	@GetMapping("/getMessage")
-	public String get() {
-		return " Hloo mann !";
+	public void get() {
+	System.out.println("test");
 	}
 
 }
